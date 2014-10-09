b56ce287a94d6ae88365dcdc0aac37514d468e9c
jrupesh <j.rupesh@gmail.com>
2014-10-09 15:42:30 +0530

    Skip inclusion filter.

--- lib/redmine_workflow_hidden_fields/workflow_permission_patch.rb
+++ lib/redmine_workflow_hidden_fields/workflow_permission_patch.rb
@@ -2,23 +2,15 @@ module RedmineWorkflowHiddenFields
   module  WorkflowPermissionPatch
     def self.included(base)
       base.send(:include, InstanceMethods)
-      #base.extend(ClassMethods)
       base.class_eval do
 
-        # HACK: Remove the existing validates_uniqueness_of block
-       #
+        # The filters are part of validators are raw, they can be skipped with the following way.
+        rule_inclusion_validation = base._validators[:rule].find{ |validator| validator.is_a? ActiveModel::Validations::InclusionValidator }
+        base._validators[:rule].delete(rule_inclusion_validation)
+        filter = base._validate_callbacks.find{ |c| c.raw_filter == rule_inclusion_validation }.filter
+        skip_callback :validate, filter            
 
-        # Add new validates_uniqueness_of with correct scope
-        #validates_uniqueness_of :name, :case_sensitive => false, :scope => :site_id
-      
-
-
-        #validates_inclusion_of :rule, :in => %w(readonly required hidden)
-        _validators.reject!{ |key, _| key == :rule }
-        
-        #_validate_callbacks.reject! do |callback|
-        #  callback.raw_filter.attributes == [:rule]
-        #end
+        validates_inclusion_of :rule, :in => %w(readonly required hidden)
       end
     end
 
