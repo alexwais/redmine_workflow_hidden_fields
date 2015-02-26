1462d2f11f1df8ec69800df4ee255115f50dd8a8
Guillaume Michalag <gmichalag@virtua.ch>
2015-02-26 11:16:52 +0100

    fix 2.6 compatibility

--- lib/redmine_workflow_hidden_fields/workflows_helper_patch.rb
+++ lib/redmine_workflow_hidden_fields/workflows_helper_patch.rb
@@ -11,30 +11,34 @@ module RedmineWorkflowHiddenFields
 
     module InstanceMethods
 
-      def field_permission_tag_with_hidden(permissions, status, field, roles)
-        name = field.is_a?(CustomField) ? field.id.to_s : field
-        options = [["", ""], [l(:label_readonly), "readonly"]]
-        options << [l(:label_hidden), "hidden"]
-        options << [l(:label_required), "required"] unless field_required?(field)
-        html_options = {}
-	if perm = permissions[status.id][name]
-          if perm.uniq.size > 1 || perm.size < @roles.size * @trackers.size
-            options << [l(:label_no_change_option), "no_change"]
-            selected = 'no_change'
-          else
-            selected = perm.first
-          end
-        end
+        def field_permission_tag_with_hidden(permissions, status, field, roles)
+            name = field.is_a?(CustomField) ? field.id.to_s : field
+            options = [["", ""], [l(:label_readonly), "readonly"]]
+            options << [l(:label_hidden), "hidden"]
+            options << [l(:label_required), "required"] unless field_required?(field)
+            html_options = {}
 
-        hidden = field.is_a?(CustomField) && !field.visible? && !role.custom_fields.to_a.include?(field)
-        if hidden
-          options[0][0] = l(:label_hidden)
-          selected = ''
-          html_options[:disabled] = true
-        end
+            if perm = permissions[status.id][name]
+              if perm.uniq.size > 1 || perm.size < @roles.size * @trackers.size
+                options << [l(:label_no_change_option), "no_change"]
+                selected = 'no_change'
+              else
+                selected = perm.first
+              end
+            end
 
-	select_tag("permissions[#{status.id}][#{name}]", options_for_select(options, selected), html_options)
-      end
+            hidden = field.is_a?(CustomField) &&
+              !field.visible? &&
+              !roles.detect {|role| role.custom_fields.to_a.include?(field)}
+
+            if hidden
+              options[0][0] = l(:label_hidden)
+              selected = ''
+              html_options[:disabled] = true
+            end
+
+            select_tag("permissions[#{status.id}][#{name}]", options_for_select(options, selected), html_options)
+        end
 
     end
   end
