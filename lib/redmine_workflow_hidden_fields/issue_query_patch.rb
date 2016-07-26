75917f43327480c63cc297846fe22a8576887748
Victor Campos <victor.campos@visagio.com>
2016-07-26 12:09:11 -0300

    remove hidden from queries

--- lib/redmine_workflow_hidden_fields/issue_query_patch.rb
+++ lib/redmine_workflow_hidden_fields/issue_query_patch.rb
@@ -4,9 +4,8 @@ module RedmineWorkflowHiddenFields
 			base.send(:include, InstanceMethods)
 			base.class_eval do
 				unloadable
-				#alias_method_chain :initialize_available_filters, :hidden
-				alias_method_chain :available_columns, :hidden
-				alias_method_chain :available_filters, :hidden
+				# alias_method_chain :available_columns, :hidden
+				# alias_method_chain :available_filters, :hidden
 			end
 		end
 
@@ -15,18 +14,18 @@ module RedmineWorkflowHiddenFields
 			def available_columns_with_hidden
 				unless @available_columns
 					@available_columns = available_columns_without_hidden
-					fields = completely_hidden_fields.map {|field| field.sub(/_id$/, '')}  
+					fields = completely_hidden_fields.map {|field| field.sub(/_id$/, '')}
 
 					@available_columns.reject! {|column|
 						fields.include?(column.name.to_s)
-					} 
+					}
 				end
 				@available_columns
 			end
-			
+
 
 			def available_filters_with_hidden
-				unless @available_filters				
+				unless @available_filters
 					@available_filters = available_filters_without_hidden
 					completely_hidden_fields.each {|field|
 						delete_available_filter field
@@ -36,7 +35,7 @@ module RedmineWorkflowHiddenFields
 						if field == "assigned_to_id" then
 							delete_available_filter "member_of_group"
 						end
-					}					
+					}
 				end
 				@available_filters
 			end
