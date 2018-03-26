1d212b51d0683d6804330cb633b2c9e0cf02337a
Alex Wais <alexwais@users.noreply.github.com>
2018-03-26 10:41:52 +0200

    Bump version

--- init.rb
+++ init.rb
@@ -29,7 +29,7 @@ Redmine::Plugin.register :redmine_workflow_hidden_fields do
   name 'Redmine Workflow Hidden Fields plugin'
   author 'Alexander Wais, David Robinson, et al.'
   description "Provides a 'hidden' issue field permission for workflows"
-  version '0.4.0'
+  version '0.5.0'
   url 'https://github.com/alexwais/redmine_workflow_hidden_fields'
   author_url 'http://www.redmine.org/issues/12005'
 end
