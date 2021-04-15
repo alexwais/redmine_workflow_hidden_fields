d519e571a24c46ac15ffaf279014d3a7964f9946
Brice Beaumesnil <brice.beaumesnil@douane.finances.gouv.fr>
2021-04-15 16:35:52 +0200

    redmine 4.2.0 + redmine_risks compatibility

--- lib/redmine_workflow_hidden_fields/journal_patch.rb
+++ lib/redmine_workflow_hidden_fields/journal_patch.rb
@@ -4,8 +4,12 @@ module RedmineWorkflowHiddenFields
 		def visible_details(user=User.current)
 			dets = super				
 			dets.select do |detail|
-				if detail.property == 'attr' or detail.property == 'cf'
-					!issue.hidden_attribute?(detail.prop_key, user)
+				unless issue.nil?
+					if detail.property == 'attr' or detail.property == 'cf'
+						!issue.hidden_attribute?(detail.prop_key, user)
+					else
+						true
+					end
 				else
 					true
 				end
