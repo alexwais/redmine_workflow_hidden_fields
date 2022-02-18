99e818ed5ecc240edaeeddaab4aab2284c6117a7
Alex Wais <alexwais@users.noreply.github.com>
2022-02-18 03:20:22 +0100

    Revert "higher unless action"
    This reverts commit 39f4e0631c5ec699263252b246c1e6178fcca55b.

--- lib/redmine_workflow_hidden_fields/journal_patch.rb
+++ lib/redmine_workflow_hidden_fields/journal_patch.rb
@@ -2,17 +2,17 @@ module RedmineWorkflowHiddenFields
 	module  JournalPatch
 		# Returns journal details that are visible to user
 		def visible_details(user=User.current)
-			unless issue.nil?
-				dets = super				
-				dets.select do |detail|
+			dets = super				
+			dets.select do |detail|
+				unless issue.nil?
 					if detail.property == 'attr' or detail.property == 'cf'
 						!issue.hidden_attribute?(detail.prop_key, user)
 					else
 						true
 					end
+				else
+					true
 				end
-			else
-				true
 			end						
 		end
 	end
