715eff1549b6563e66d4840950caa4521268b6a2
Cire DIA <ciremoussadia@gmail.com>
2022-11-15 11:04:37 +0000

    Connect job to a workflow permission callback

--- lib/redmine_workflow_hidden_fields/workflow_permission_patch.rb
+++ lib/redmine_workflow_hidden_fields/workflow_permission_patch.rb
@@ -6,10 +6,15 @@ module RedmineWorkflowHiddenFields
         rule_inclusion_validation = base._validators[:rule].find{ |validator| validator.is_a? ActiveModel::Validations::InclusionValidator }
         base._validators[:rule].delete(rule_inclusion_validation)
         filter = base._validate_callbacks.find{ |c| c.raw_filter == rule_inclusion_validation }.filter
-        skip_callback :validate, filter            
+        skip_callback :validate, filter
 
         validates_inclusion_of :rule, :in => %w(readonly required hidden)
+        after_commit :update_hidden_attribute_names
       end
+    end
+
+    def update_hidden_attribute_names
+      HiddenAttributeNamesJob.perform_later
     end
   end
 end
